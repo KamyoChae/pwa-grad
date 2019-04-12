721f1b6db6f5014a47aa9e563c9e0fe19890a870
kamyochae <1465678596@qq.com>
2019-04-12 15:39:33 +0800

    pwa config

---
+++ .lavas/skeleton.js
@@ -0,0 +1,19 @@
+import Vue from 'vue';
+
+import Skeleton from 'F:\\worker\\graduation\\pwa-grad\\core\\Skeleton.vue';
+
+
+export default new Vue({
+    components: {
+        
+        Skeleton,
+        
+    },
+    template: `
+        <div>
+            
+            <skeleton id="skeleton" style="display:none"/>
+            
+        </div>
+    `
+});
